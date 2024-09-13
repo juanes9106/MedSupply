ad97139f935eb5fb6fd39f7b5c0fd075d2401b03
Esteban Vargas <juanes9106@gmail.com>
2024-09-12 22:45:00 -0500

    Nueva conexión a base de datos

--- database.js
+++ database.js
@@ -1,28 +1,32 @@
 const { Connection, Request, TYPES } = require('tedious');
+require('dotenv').config();
 
+
+// Configuración de la conexión
 const config = {
-  server: "DESKTOP-0LC768E\\SQLEXPRESS",
-  database: "MyDatabase",
-  requestTimeout: 30000,
-  connectionTimeout: 30000,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
   options: {
-    trustServerCertificate: true,
+    trustServerCertificate: true, // Para desarrollo; usar certificados en producción
   },
   authentication: {
     type: "default",
     options: {
-      userName: "juanes9106",
-      password: "geminis9106",
+      userName: process.env.DB_USER, // Usa variables de entorno
+      password: process.env.DB_PASSWORD,
     }
   }
 };
 
+// Conectar a la base de datos
 function connectToDatabase(callback) {
   const connection = new Connection(config);
 
+  console.log(`Connecting to database ${config.database} on server ${config.server}`);
+
   connection.on('connect', function(err) {
     if (err) {
-      console.log('Error: ', err);
+      console.error('Error connecting to database: ', err);
       callback(err, null);
     } else {
       console.log('Connected to SQL Server...');
@@ -33,21 +37,24 @@ function connectToDatabase(callback) {
   connection.connect();
 }
 
+// Autenticar usuario
 function authenticateUser(username, password, callback) {
   connectToDatabase((err, connection) => {
     if (err) {
-      console.log('Error connecting to database: ', err);
       callback(err, null);
       return;
     }
+
+    //console.log(`Authenticating user: ${username}`);
 
     const request = new Request(
       `SELECT * FROM dbo.Users WHERE username = @username AND pass = @pass`,
       (err, rowCount) => {
         if (err) {
-          console.log('Error executing query: ', err);
+          console.error('Error executing query: ', err);
           callback(err, null);
         } else {
+          //console.log(`Query executed, row count: ${rowCount}`);
           callback(null, rowCount > 0);
         }
         connection.close(); // Cerramos la conexión después de ejecutar la consulta
--- login.js
+++ login.js
@@ -7,10 +7,8 @@ document.getElementById('loginForm').addEventListener('submit', (event) => {
 
     ipcRenderer.send('login', { username, password });
 
-    ipcRenderer.on('login-reply', (event, result) => {
+    ipcRenderer.once('login-reply', (event, result) => {
         if (result.success) {
-            alert('Login successful!');
-            // Redirigir a la página principal o realizar otra acción
             mainWindow.loadFile('index.html');  
         } else {
             alert('Login failed: ' + result.message);
